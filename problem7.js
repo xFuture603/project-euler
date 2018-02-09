05c397fa913f466806409c574840622d81ae2482
xFuture603 <daniel.xfuture@googlemail.com>
2018-02-09 14:28:32 +0100

    fixed problem 7
    ...

--- problem7.js
+++ problem7.js
@@ -9,21 +9,15 @@
     var j = 2;
     var maxPrimes = 10001;
   
-    // while primes is less than the sought after number
     while (primes.length < maxPrimes) {
-      // while i divided by j has a remainder,
-      // indicating it could still be a prime
       while (i % j !== 0) {
         j++;
-        // if we're reached the point where j === i,
-        // then it must be a prime
         if ( j === i ) {
           primes.push(i);
           break;
         }
       }
   
-      // otherwise, increment i and reset j;
       i++;
       j = 2;
     }
